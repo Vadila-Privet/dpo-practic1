0c585d4f9662d5ddee6854d97cbf3c13184c00fa
Vasilii Stroev <stroev422@gmmail.com>
2023-12-18 22:00:00 +0300

    Треугольник из Х-ов

---
+++ task 1.py
@@ -0,0 +1,12 @@
+my_number = "X"
+print('Мой вариант - ', my_number)
+height = 3
+i = 0
+while (i < height):
+    j=0
+    while(j <= i):
+        print('X', end="")
+        j=j+1
+    print("")
+    i = i+1
+
